099eaf007861306999c91a24d8ff9321bd19bdfa
mondalr3 <rodoshi.mondal@mail.utoronto.ca>
2024-12-15 13:31:52 -0500

    Implemented Model and controller to display mapInfo as a JSON response on a web browser

--- src/main/java/com/rodoshi/googlemapsAPI/Model/MapData.java
+++ src/main/java/com/rodoshi/googlemapsAPI/Model/MapData.java
@@ -1,4 +1,57 @@
 package com.rodoshi.googlemapsAPI.Model;
-
 public class MapData {
+    double latitude;
+    double longitude;
+    int zoom;
+    String location;
+
+
+    public MapData(double latitude, double longitude, int zoom, String location){
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.zoom = zoom;
+        this.location = location;
+
+    }
+
+    public double getLatitude(){
+        return this.latitude;
+    }
+
+    public double getLongitude(){
+        return this.longitude;
+    }
+    public double getZoom(){
+        return this.zoom;
+    }
+
+    public String getLocation(){
+        return this.location;
+    }
+
+    public double setLatitude(double latitude){
+        return this.latitude = latitude;
+    }
+
+    public double setLongitude(double longitude){
+        return this.longitude = longitude;
+    }
+
+    public double setZoom(int zoom){
+        return this.zoom = zoom;
+    }
+
+    public String setLocation(String location){
+        return this.location = location;
+    }
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/com/rodoshi/googlemapsAPI/controller/MapController.java
+++ src/main/java/com/rodoshi/googlemapsAPI/controller/MapController.java
@@ -1,4 +1,44 @@
 package com.rodoshi.googlemapsAPI.controller;
+import com.rodoshi.googlemapsAPI.Model.MapData;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+// This marks the class as a controller for Spring boot (framework being used)
+// Takes care of mapping request data to the handler method
+@RestController
 public class MapController {
+
+    public MapController(){
+    }
+
+    @GetMapping("/api/maps")
+    /* This annotation maps HTTP GET requests in this handler method in the Spring controller class
+    (api/maps) is the URL endpoint that the backend listens to when a request is sent to this URL.
+    A request could be http://localhost:8080/api/maps since we know Tomcat is running on port 8080
+    When a request is sent to this URL, getMapInfo() is called
+     */
+
+    public List<MapData> getMapInfo(){
+        List<MapData> mapDataList = new ArrayList<MapData>();
+        MapData m1 = new MapData(43.6532, -79.3832, 10, "Toronto");
+        MapData m2 = new MapData(76.2112, -89.3231, 10, "Pickering");
+        mapDataList.add(m1);
+        mapDataList.add(m2);
+        return mapDataList;
+
+    }
+
+
+
+
+
+
+
+
+
 }
